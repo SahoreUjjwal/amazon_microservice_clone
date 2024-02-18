46bd34accb02a1d75db74cb7da66a39f8609824b
ujjwal sahore <ujjwalsahorework@gmail.com>
2024-02-18 19:30:08 +0530

    Added three microservices and a gateway to all the microservices

---
+++ product/index.js
@@ -0,0 +1,11 @@
+const express =require("express");
+const app = express();
+app.use(express.json());
+
+app.use('/',(req,res,next)=>{
+    return res.status(200).json({"msg":"Hello from product"})
+})
+
+app.listen(8002,()=>{
+    console.log("product service started at port 8002");
+})
